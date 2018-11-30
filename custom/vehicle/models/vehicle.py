0b82f8e783901592d10c91103066d9c8b734a9c1
tramm <ram.tangirala@gmail.com>
2018-11-30 20:09:38 +0530

    updated vehicle module

--- custom/vehicle/models/vehicle.py
+++ custom/vehicle/models/vehicle.py
@@ -5,7 +5,7 @@ from odoo import api, fields, models, _
 from odoo.exceptions import UserError
 
 
-class ProductionLot(models.Model):
+class Vehicle(models.Model):
     _name = 'vehicle'
     _inherit = 'stock.production.lot'
     _description = 'Vehicle'
@@ -15,8 +15,9 @@ class ProductionLot(models.Model):
     chassis_no = fields.Char('Chasis Number',help = "Unique Chasis number of the vehicle")
     order_id = fields.Many2one(
         'sale.order', 'SaleOrder',
-        domain=[('state', 'in', ['sale', 'draft'])], required=True)
+        domain=[('state', 'in', ['sale','draft'])], required=True)
     battery_no = fields.Char('Battery Number',help = "Unique Battery number of the vehicle")
+    customer_id = fields.Many2one('res.partner', string='Customer', readonly=True, compute='_get_customer')
 
     @api.model_create_multi
     def create(self, vals_list):
@@ -25,7 +26,7 @@ class ProductionLot(models.Model):
             picking_id = self.env['stock.picking'].browse(active_picking_id)
             if picking_id and not picking_id.picking_type_id.use_create_lots:
                 raise UserError(_('You are not allowed to create a lot or serial number with this operation type. To change this, go on the operation type and tick the box "Create New Lots/Serial Numbers".'))
-        return super(ProductionLot, self).create(vals_list)
+        return super(Vehicle, self).create(vals_list)
 
     @api.multi
     def write(self, vals):
@@ -37,10 +38,11 @@ class ProductionLot(models.Model):
                     'if some stock moves have already been created with that number. ' +
                     'This would lead to inconsistencies in your stock.'
                 ))
-        return super(ProductionLot, self).write(vals)
+        return super(Vehicle, self).write(vals)
 
     @api.one
-    def _product_qty(self):
-        # We only care for the quants in internal or transit locations.
-        quants = self.quant_ids.filtered(lambda q: q.location_id.usage in ['internal', 'transit'])
-        self.product_qty = sum(quants.mapped('quantity'))
+    def _get_customer(self):
+        # We only care for the customer if sale order is entered.
+        if self.order_id:
+            customer_id = self.order_id.partner_id
+            self.customer_id = customer_id
